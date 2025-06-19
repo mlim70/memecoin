b707cf912483f053f48f8a3c1e790b8c0acdc11c
Matthew Lim <kmmatthew8@gmail.com>
2025-06-19 03:15:29 -0700

    adjusted nav bar style

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import HomePage from './pages/HomePage'
 import DashboardPage from './pages/DashboardPage'
 import LeaderboardPage from './pages/LeaderboardPage'
 import AccountPage from './pages/AccountPage'
-import { UserProvider } from './contexts/UserContext'
 
 function App() {
   return (
-    <UserProvider>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
-          <Route path="/account" element={<AccountPage />} />
-        </Routes>
-      </div>
-    </UserProvider>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
+    </div>
   )
 }
 
