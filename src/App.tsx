b4078f2b77eec0ce182b65a4a557f0ec5b234ff6
matt.liorabios <matt.lim@liorabios.com>
2025-06-19 03:11:14 -0700

    removed google OAuth

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import LeaderboardPage from './pages/LeaderboardPage'
 import AccountPage from './pages/AccountPage'
@@ -14,7 +13,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/leaderboard" element={<LeaderboardPage />} />
           <Route path="/account" element={<AccountPage />} />
